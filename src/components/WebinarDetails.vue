06c66ad925b5b8416933910e6b1c515c7edde520
jaciewyatt <jaciew@rams.colostate.edu>
2021-12-11 10:37:03 -0700

    15.2 12/2 /webinar/:pk

--- src/components/WebinarDetails.vue
+++ src/components/WebinarDetails.vue
@@ -1,11 +1,55 @@
 <template>
   <div>
-    <h1>Webinar Details</h1>
+    <div class="card">
+      <div class="card-body">
+        <h2 class="text-primary">{{ webinar.Title }}</h2>
+        <br />
+
+        <p>
+          Zoom Link: <br /><strong>{{ webinar.ZoomLink }}</strong>
+        </p>
+        <p>
+          Date: <br /><strong>{{ formattedDate }}</strong>
+        </p>
+        <p>
+          Length (min)<br /><strong>{{ webinar.Length }}</strong>
+        </p>
+      </div>
+    </div>
+    <br />
+
+    <router-link v-if="auth" :to="`/webinars/${this.$route.params.pk}/register`"
+      ><button class="btn btn-success">Register</button>
+    </router-link>
+
+    <router-link v-else :to="`/login`"
+      ><button class="btn btn-outline-success">Signin to Register</button>
+    </router-link>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  computed: {
+    webinar() {
+      let allWebinars = this.$store.state.webinars;
+
+      let thisWebinar = allWebinars.find((aWebinar) => {
+        return aWebinar.WebinarPK == this.$route.params.pk;
+      });
+      return thisWebinar;
+    },
+    formattedDate() {
+      return new Intl.DateTimeFormat("en-GB", {
+        dateStyle: "full",
+        timeStyle: "long",
+      }).format(this.webinar.Date);
+    },
+    auth() {
+      return this.$store.state.token;
+    },
+  },
+};
 </script>
 
 <style></style>
